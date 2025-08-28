acbad030887c1e65eb61fe220dfb0c8332b69304
lorrainysilva <lorrainyalves786@gmail.com>
2025-08-28 01:38:57 -0300

    adicionando com a uml

---
+++ code/entity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
+
+def entity():
+    return None
